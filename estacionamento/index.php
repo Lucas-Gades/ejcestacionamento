aeb1aaed4905cff782d895bffe49c246998e66a0
Lucas-Gades <70823238+Lucas-Gades@users.noreply.github.com>
2020-11-06 03:40:00 -0300

    Add files via upload

---
+++ estacionamento/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!--Tags básicas do head-->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Arrays em PHP</title>
+    <!--Link dos nossos arquivos CSS e JS padrão-->
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <script src="js/scripts.js"></script>
+    <!--Link dos arquivos CSS e JS do Bootstrap-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body id="bodyindex">
+
+    <?php
+
+    include "box.php";
+
+    include_once "boxDAO.php";
+
+    include_once "usuarioDAO.php";
+
+    $con = Conexao::getConexao();
+
+    session_start();
+
+
+
+    $_SESSION["logado"] = false;
+
+    if (isset($_POST["entrar"])) {
+        $usuario = $_POST["txtUsuario"];
+        $senha = $_POST["txtSenha"];
+
+        if (UsuarioDAO::logar($usuario, $senha)) {
+            $_SESSION["logado"] = true;
+            session_cache_expire(10);
+            header("Location: listagem.php");
+        }
+    }
+
+
+
+    ?>
+
+    <div class="container">
+      
+
+        <div class=" d-flex justify-content-center  ">
+
+            <div class="card " >
+                <p ><h3 style="color: white;text-align: center;">EJC.Estacionamento </h3></p>
+                <div class="card-header">
+                    <div class="col-md-6"><img src='img/carro.png' style='height:70%; width:80%; margin-left:130px;'></div>
+
+
+                    <div class="col-md-4"><h3>Login</h3></div>
+                    
+                    <div class="d-flex justify-content-end social_icon">
+                        <span><i class="fab fa-facebook-square"></i></span>
+                        <span><i class="fab fa-google-plus-square"></i></span>
+                        <span><i class="fab fa-twitter-square"></i></span>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form method="post" action="index.php">
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type='text' name="txtUsuario" value="" class="form-control" placeholder="username">
+
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type='password' name="txtSenha" value="" class="form-control" placeholder="password">
+                        </div>
+                        <div class="row align-items-center remember">
+                            <input type="checkbox">Remember Me
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="enter" name="entrar" class="btn float-right login_btn">
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
